edcd9fb9ee4980f5c536b8ce6d0cb15b932a27ce
jeremy <collyerdesign@gmail.com>
2016-05-08 07:50:44 -0400

    test 2

--- src/main.js
+++ src/main.js
@@ -5,23 +5,12 @@ then use React and React-DOM to render it.
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Router = require('react-router').Router;
-var hashHistory = require('react-router').hashHistory;
-var Provider = require('react-redux').Provider;
-var store = require('./store');
-var routes = require('./routes');
-var actions = require('./actions');
+
 var App = require('./app.js');
-require('./style/styles.css');
+
 
 ReactDOM.render(
 
 	<App />,
 	document.getElementById("root")
 );
-
-// setup Firebase listeners
-setTimeout(function(){
-	store.dispatch( actions.startListeningToAuth() );
-	store.dispatch( actions.startListeningToTracks() );
-});
