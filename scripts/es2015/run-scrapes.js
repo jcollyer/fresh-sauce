77c0409a7863169d920992540d441815564aa808
jeremy <collyerdesign@gmail.com>
2016-06-16 07:54:46 -0400

    move web scripts into separate folder

--- scripts/es2015/run-scrapes.js
+++ scripts/es2015/run-scrapes.js
@@ -1,7 +1,7 @@
-import { anonymouslygifted } from './anonymouslygifted.js'
-import { bound2hiphop } from './bound2hiphop.js'
-import { carolinaontherise } from './carolinaontherise.js'
-import { goodmusicallday } from './goodmusicallday.js'
+import { anonymouslygifted } from './websites/anonymouslygifted.js'
+import { bound2hiphop } from './websites/bound2hiphop.js'
+import { carolinaontherise } from './websites/carolinaontherise.js'
+import { goodmusicallday } from './websites/goodmusicallday.js'
 
 anonymouslygifted()
 bound2hiphop()
--- scripts/es5/run-scrapes.js
+++ scripts/es5/run-scrapes.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var _anonymouslygifted = require('./anonymouslygifted.js');
+var _anonymouslygifted = require('./websites/anonymouslygifted.js');
 
-var _bound2hiphop = require('./bound2hiphop.js');
+var _bound2hiphop = require('./websites/bound2hiphop.js');
 
-var _carolinaontherise = require('./carolinaontherise.js');
+var _carolinaontherise = require('./websites/carolinaontherise.js');
 
-var _goodmusicallday = require('./goodmusicallday.js');
+var _goodmusicallday = require('./websites/goodmusicallday.js');
 
 (0, _anonymouslygifted.anonymouslygifted)();
 (0, _bound2hiphop.bound2hiphop)();
