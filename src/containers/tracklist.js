44be2416d7eaacf53946118bd46e3831b241f922
jeremy <collyerdesign@gmail.com>
2016-07-05 20:53:48 -0400

    format tracklist

--- src/containers/tracklist.js
+++ src/containers/tracklist.js
@@ -4,27 +4,27 @@ import { setTrack } from '../actions/tracks'
 import Track from '../components/track'
 
 class Tracklist extends Component {
- 	render() {
-   	const { tracks, hasreceiveddata, player } = this.props
+  render() {
+    const { tracks, hasreceiveddata, player } = this.props
     const rows = tracks.map((track) => {
-   	  return <Track track={track} key={track.id} onPlayTrackClicked={() => this.props.setTrack(track, player)} />
+      return <Track track={track} key={track.id} onPlayTrackClicked={() => this.props.setTrack(track, player)} />
     }).reverse()
 
     return (
-   	  <div id="tracklist" className={player ? "player-padding" : ""}>
-      {hasreceiveddata ? rows : "Loading tracks..."}
+      <div id="tracklist" className={player ? "player-padding" : ""}>
+        {hasreceiveddata ? rows : "Loading tracks..."}
       </div>
     )
   }
 }
 
 const mapStateToProps = (appState) => {
-	return {
+  return {
     tracks: appState.tracks.tracks,
     hasreceiveddata: appState.tracks.hasreceiveddata,
     auth: appState.auth,
     player: appState.tracks.player
-	}
+  }
 }
 
 export default connect(mapStateToProps, {setTrack})(Tracklist)
