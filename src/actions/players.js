ae1a56e5688ffb5641229029bdc5d9a082d62319
jeremy <collyerdesign@gmail.com>
2016-07-17 09:05:36 -0400

    move player setup into action

--- src/actions/players.js
+++ src/actions/players.js
@@ -3,32 +3,50 @@ import store from '../store'
 import { playNextTrack } from '../actions/tracks'
 
 function setSoundCloud(){
-	//get Soundcloud Widget
-	let playerSC = SC.Widget(document.getElementById('soundcloud_widget'))
-	playerSC.bind(SC.Widget.Events.READY, () => {
-		console.log('SC Widget Ready...')
-	})
-	playerSC.bind(SC.Widget.Events.FINISH, () => {
-		store.dispatch(playNextTrack())
-		console.log('play next track from -- SC')
-	})
-	return playerSC
+  //get Soundcloud Widget
+  let playerSC = SC.Widget(document.getElementById('soundcloud_widget'))
+  playerSC.bind(SC.Widget.Events.READY, () => {
+    console.log('SC Widget Ready...')
+  })
+  playerSC.bind(SC.Widget.Events.FINISH, () => {
+    store.dispatch(playNextTrack())
+    console.log('play next track from -- SC')
+  })
+  return playerSC
 }
 
 function setYouTube(){
-	//get YouTube Widget
-	let playerYT = new YT.Player('yt_widget', {
-		height: '100',
-		width: '200',
-		videoId: '2a4Uxdy9TQY', //some randome ID
-		playerVars: { 'autoplay': 0, 'controls': 0,'autohide':1,'wmode':'opaque' }
-	})
-	return playerYT
+
+  // function onPlayerReady(event) {
+  //    that.whileTrackPlaying(event.target)
+  //    oldTrackId = track.id
+  //  }
+
+   function onPlayerStateChange(event) {
+     switch(event.data) {
+       case 0: // video finished
+         store.dispatch(playNextTrack())
+       default: return console.log("updateYTPlayer", event.data)
+     }
+   }
+
+  //get YouTube Widget
+  let playerYT = new YT.Player('yt_widget', {
+    height: '100',
+    width: '200',
+    videoId: '2a4Uxdy9TQY', //some randome ID
+    playerVars: { 'autoplay': 0, 'controls': 0,'autohide':1,'wmode':'opaque' },
+    events: {
+      // 'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  })
+  return playerYT
 }
 
 export function setPlayers() {
-	let playerSC = setSoundCloud()
-	let playerYT = setYouTube()
+  let playerSC = setSoundCloud()
+  let playerYT = setYouTube()
   let playerOptions = { playerSC, playerYT }
 
   return { type: C.SET_PLAYERS, playerOptions }
