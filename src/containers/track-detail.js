1ad696c8744a9ea17edf7c61325ba24907df1e22
jcollyer <collyerdesign@gmail.com>
2016-07-23 07:52:28 -0700

    set track detail - reduxify

--- src/containers/track-detail.js
+++ src/containers/track-detail.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
+import { setTrackDetail, setTrack } from '../actions/tracks'
 
 class TrackDetail extends Component {
+  componentDidMount(){
+    let id = this.props.routeParams.id
+    this.props.setTrackDetail(id)
+  }
   render() {
-    return <i>hi there track detail page!</i>
+    const { currentTrack } = this.props
+    if (currentTrack.id) {
+      return (
+        <div>
+          <h2>{currentTrack.title}</h2>
+          <img src={currentTrack.artwork_url} onClick={() => this.props.setTrack(currentTrack)} />
+        </div>
+      )
+    } else {
+      return <i>hi there track detail page!</i>
+    }
   }
 }
 
 const mapStateToProps = (appState) => {
   return {
-    tracklist: appState.tracklist.tracks,
-    hasreceiveddata: appState.tracklist.hasreceiveddata,
-    trackPlaying: appState.tracks.trackPlaying,
-    auth: appState.auth
+    currentTrack: appState.tracks.currentTrack
   }
 }
 
-export default withRouter(connect(mapStateToProps)(TrackDetail))
+export default withRouter(connect(mapStateToProps, { setTrackDetail, setTrack })(TrackDetail))
