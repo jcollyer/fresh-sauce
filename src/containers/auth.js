dfca9e8d40e658f0b6d4c945bd6046ccd6931c57
jcollyer <collyerdesign@gmail.com>
2016-07-24 15:18:00 -0700

    update login

--- src/containers/auth.js
+++ src/containers/auth.js
@@ -1,53 +1,30 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { openAuth, logoutUser } from '../actions/auth'
-import C from '../constants'
 
-class Auth extends Component {
-  getJSX(props) {
-    switch (props.auth.status) {
-      case C.AUTH_LOGGED_IN: return (
-        <div>
-          <span>Logged in as {props.auth.username}.</span>
-          {" "}<button onClick={props.logoutUser}>Log out</button>
-        </div>
-      )
-      case C.AUTH_AWAITING_RESPONSE: return (
-        <div>
-          <button disabled>authenticating...</button>
-        </div>
-      )
-      default: return (
-        <div>
-          <button onClick={props.openAuth}>Log in</button>
-          
-          <p>Log in with your Facebook account below.</p>
-
-          <div className="quickstart-user-details-container">
-            Firebase sign-in status: <span id="quickstart-sign-in-status">Unknown</span>
-            <div>Firebase auth <code>currentUser</code> object value:</div>
-            <pre><code id="quickstart-account-details">null</code></pre>
-            <div>Facebook OAuth Access Token:</div>
-            <pre><code id="quickstart-oauthtoken">null</code></pre>
-          </div>
-        </div>
-      )
-    }
-  }
+export default class Auth extends Component {
   render() {
-    return this.getJSX(this.props)
+
+    var uiConfig = {
+      'signInSuccessUrl': '/',
+      'signInOptions': [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+       //  firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ]
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+    return (
+      <div>
+        <h1>Welcome to My Awesome App</h1>
+        <div id="firebaseui-auth-container"></div>
+      </div>
+    )
   }
 }
-
-const mapStateToProps = (appState) => {
-  return {
-    auth: appState.auth
-  }
-}
-
-const mapDispatchToProps = {
-  openAuth,
-  logoutUser,
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Auth)
