0e41feb42a87ff4efe8c563ffa762aa856684a33
jcollyer <collyerdesign@gmail.com>
2016-07-30 16:18:44 -0400

    add sc/yt link on each track, update icon-font

--- src/components/track.js
+++ src/components/track.js
@@ -2,11 +2,21 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default (props) => {
-
-  //set player track artist depending if its a SC or YT track
-  var trackArtist = ''
-  props.track && props.track.kind === 'sc' ? trackArtist = props.track.artist : trackArtist = props.track.tag_list[0]
-
+  let trackArtist = ''
+  let scURL = ''
+  let linkIcon = ''
+  let linkTitle = ''
+  if(props.track.kind === 'sc') {
+    trackArtist = props.track.artist
+    scURL = 'https://soundcloud.com/'+ props.track.artist.replace(/\s/g, '') + '/' + props.track.permalink
+    linkTitle = 'SoundCloud '
+    linkIcon = 'icon icon-soundcloud '
+  } else if (props.track.kind === 'yt') {
+    trackArtist = props.track.tag_list[0]
+    scURL = 'https://www.youtube.com/watch?v='+props.track.id
+    linkIcon = 'icon icon-youtube'
+    linkTitle = 'YouTube'
+  }
   return (
     <div className={props.isCurrentTrack ? 'track-item active' : 'track-item'}>
       <div className='track-item-info'>
@@ -16,7 +26,11 @@ export default (props) => {
         </div>
         <div className='track-item-info-detail'>
           <h3 className='title elipsis'>{props.track.title}</h3>
-          <p className='artist'><span className='track-by'>by:</span>{props.track.artist}{trackArtist}</p>
+          <p className='artist'><span className='track-by'>by: </span>{props.track.artist}{trackArtist}</p>
+          <a className='origin-link' href={scURL} target='_blank'>
+            <span>{linkTitle}</span>
+            <i className={linkIcon}></i>
+          </a>
         </div>
       </div>
       <div className='track-item-actions'>
