0320ec1716978ce3184cf8be9e7608a05ea7a0e2
jcollyer <collyerdesign@gmail.com>
2016-07-30 08:09:59 -0400

    style tracklist

--- src/components/track.js
+++ src/components/track.js
@@ -14,15 +14,14 @@ export default (props) => {
           <i className={props.isThisTrackPlaying ? 'icon icon-pause' : 'icon icon-play'}></i>
           <img src={props.track.artwork_url} />
         </div>
-        <div className='track-item-info-detail' onClick={props.onPlayTrackClicked}>
+        <div className='track-item-info-detail'>
           <h3 className='title elipsis'>{props.track.title}</h3>
-          <p className='artist'>by: {props.track.artist}{trackArtist}</p>
-          <p>{props.track.id}</p>
+          <p className='artist'><span className='track-by'>by:</span>{props.track.artist}{trackArtist}</p>
         </div>
       </div>
       <div className='track-item-actions'>
-        <i className={props.isFavoritedByUser ? 'icon icon-heart' : 'icon icon-heart-outline'} onClick={props.onToggleFavoriteTrackClicked}></i>
-        <Link to={`/tracks/${props.track.id}`}><i className='icon icon-arrow-right'></i></Link>
+        <i className={props.isFavoritedByUser ? 'track-heart icon icon-heart' : 'track-heart icon icon-heart-outline'} onClick={props.onToggleFavoriteTrackClicked}></i>
+        <Link className='go-to-track-detail' to={`/tracks/${props.track.id}`}><div className='icon icon-arrow-right'></div></Link>
         <button onClick={props.onDeleteTrackClicked} className={props.isAdmin ? '' : 'hide'}>Delete track</button>
       </div>
     </div>
