e3ac3ef973c8dc9e521ebac70c78267c39692162
jeremy collyer <collyerdesign@gmail.com>
2016-08-06 17:13:50 -0400

    load player with correct yt image for /tracks/:id

--- src/containers/player.js
+++ src/containers/player.js
@@ -39,7 +39,7 @@ class Player extends Component {
     return toHHMMSS(time) || '0:00'
   }
   componentDidMount(){
-    document.addEventListener("keydown", (e) => {
+    document.addEventListener('keydown', (e) => {
       console.log(e.keyCode)
       switch (e.keyCode) {
         case 32:
@@ -57,27 +57,30 @@ class Player extends Component {
   }
   render() {
     const { track, trackPlaying, shuffle, trackPosition, trackPercentage, players } = this.props
-    const src = "https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F" + track.id + "&show_artwork=true";
+    const src = 'https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F' + track.id + '&show_artwork=true';
     if (trackPlaying && track.id != oldTrackId) { // if track is playing, with a new track ID
       playTrack(track, players)
       this.whileTrackPlaying(players)
       oldTrackId = track.id
     }
     return (
-        <div id="player">
-          <iframe id="soundcloud_widget" width="100%" height="166" scrolling="no" frameBorder="no" src={src} className="offscreen"></iframe>
+        <div id='player'>
+          <iframe id='soundcloud_widget' width='100%' height='166' scrolling='no' frameBorder='no' src={src} className='offscreen'></iframe>
 
-          <div id="player-track-artwork">
-            <div id="yt_widget" className={track.kind === "yt" ? "" : "offscreen"}></div>
+          <div id='player-track-artwork'>
+            <div id='yt_widget' className={track.kind === 'yt' && trackPlaying === true ? '' : 'offscreen'}></div>
+            <div id='yt-thumb' className={track.kind === 'sc' ? 'hide' : ''}>
+              <img src={track.artwork_url} className={trackPlaying === true ? 'hide' : ''} />
+            </div>
             <img
               src={track.artwork_url}
-              className={track.kind === "sc" ? "" : "hide"}
+              className={track.kind === 'sc' ? '' : 'hide'}
               onClick={() => this.toggleSCTrack(players.playerSC)} />
           </div>
 
-          <div id="player-track-info">
+          <div id='player-track-info'>
             <TrackProgress progressPercentage={trackPercentage} duration={track.duration} trackProgressClick={(position) => this.trackProgressClick(position, players)} />
-            <h3 className="elipsis">{track.title}</h3>
+            <h3 className='elipsis'>{track.title}</h3>
             <h4><span id='by'>by:</span> {track.artist} </h4>
             <div id='player-track-time'>{this.convertToPrettyTime(trackPosition)}/{this.convertToPrettyTime(track.duration)}</div>
             <div id='player-track-controls'>
